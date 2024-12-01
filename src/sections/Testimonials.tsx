7c70f4b26bdb5a488e4af0959ceaba298381fb13
roshray <roshanroy759@gmail.com>
2024-12-01 15:23:55 +0530

    animation[InfinityScroll]:cards-H

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import avatar1 from "@/assets/avatar-1.png"
 import avatar2 from "@/assets/avatar-2.png"
 import avatar3 from "@/assets/avatar-3.png"
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 const testimonials = [
@@ -29,9 +32,22 @@ export const Testimonials = () => {
             <div className="container">
                 <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations.</h2>
                 <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">An Ambience hangout hub of Comics</p>
-                <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-                    <div className="flex gap-5">
-                        {testimonials.map((testimonial) => (
+                <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+                    <motion.div 
+                    
+                        initial={{
+                            translateX: "-50%",
+                        }}
+                        animate={{
+                            translateX: "0",
+                        }}
+                        transition={{
+                            repeat: Infinity,
+                            ease: "linear",
+                            duration: 30,
+                        }}
+                        className="flex gap-5  pr-5 flex-none">
+                        {[...testimonials,...testimonials].map((testimonial) => (
                             <div key={testimonial.name} 
                                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
                             >
@@ -52,7 +68,7 @@ export const Testimonials = () => {
                                 </div>
                             </div>
                         ))}
-                    </div>
+                    </motion.div>
                </div>
             </div>
         </section>
